aab6d71893d19334dfef6583e64f53b80c2bab2f
Alonso Parra <alonsogparra@gmail.com>
2019-02-05 23:24:42 -0400

    Added the react-intl elements, also added some button to make the translate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+
+import messages from './messages';
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    language: ''
+  };
+
+  spanishChangedHandler = () => {
+    this.setState({
+      language: 'es'
+    });
+  };
+
+  englishChangedHandler = () => {
+    this.setState({
+      language: 'en'
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <IntlProvider
+        locale={this.state.language}
+        messages={messages[this.state.language]}
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+
+            <h3>
+              <FormattedMessage
+                id='title.hello'
+                defaultMessage='The original message is: Hello to Everbody!!!'
+              />
+            </h3>
+
+            <div className="buttons">
+              <button onClick={this.englishChangedHandler}>English</button>
+              <button onClick={this.spanishChangedHandler}>Español</button>
+            </div>
+
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </IntlProvider>
     );
   }
 }
